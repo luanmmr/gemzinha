c3adac13399bcc26ea16a4632e8bd58a5d958fb4
Luan Ribeiro <luan.mesquita@hotmail.com>
2023-03-04 17:12:57 +0000

    Added new tests to food class

--- lib/gemzinha/food.rb
+++ lib/gemzinha/food.rb
@@ -1,3 +1,5 @@
+require 'active_support/inflector'
+
 module Gemzinha
   class Food
     def self.portray(food)
@@ -6,6 +8,10 @@ module Gemzinha
       else
         "Delicious!"
       end
+    end
+
+    def self.pluralize(word)
+      word.pluralize
     end
   end
 end
--- spec/gemzinha_spec.rb
+++ spec/gemzinha_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Gemzinha do
   it "anything else is delicious" do
     expect(Gemzinha::Food.portray("Not Broccoli")).to eql("Delicious!")
   end
+
+  it "pluralizes a word" do
+    expect(Gemzinha::Food.pluralize("Tomato")).to eql("Tomatoes")
+  end
 end
